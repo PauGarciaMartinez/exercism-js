ce6edef60a5bcf51d222a4a99c4474831b526115
Pau Garcia Martínez <73617104+PauGarciaMartinez@users.noreply.github.com>
2021-11-09 20:37:05 +0100

    Solution to ozans-playlist exercise

---
+++ learning-exercises/ozans-playlist.js
@@ -0,0 +1,23 @@
+export function removeDuplicates(playlist) {
+  return [...new Set(playlist)];
+}
+
+export function hasTrack(playlist, track) {
+  return (new Set(playlist).has(track));
+}
+
+export function addTrack(playlist, track) {
+  const set = new Set(playlist);
+  set.add(track);
+  return [...set];
+}
+
+export function deleteTrack(playlist, track) {
+  const set = new Set(playlist);
+  set.delete(track);
+  return [...set];
+}
+
+export function listArtists(playlist) {
+  return [...new Set(playlist.map(val => val.split(' - ')[1]))];
+}
